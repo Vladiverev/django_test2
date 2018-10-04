819bb51e6a6005bd16db14f56995b08c2851cb17
vladiverev <vladiverev@gmail.com>
2018-10-05 00:36:26 +0300

    mod

--- country_info/models.py
+++ country_info/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+
+class Country(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
+class City(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    country_id = models.ForeignKey(Country, on_delete=models.PROTECT)
+    title = models.CharField(max_length=200)
+    desc = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
