d5f6439e1a7c2cba023aafff5409386b8ac2bc0e
vladiverev <vladiverev@gmail.com>
2018-10-04 17:07:12 +0300

    1st

---
+++ testing/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\walmiors\test_project\py_test\django_test2\testing\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
